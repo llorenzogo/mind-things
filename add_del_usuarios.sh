8870960061580f2e20d13d9b72150cb2e7946136
llorenzogo <34598954+llorenzogo@users.noreply.github.com>
2018-02-27 20:44:09 +0100

    Update add_del_usuarios.sh

--- add_del_usuarios.sh
+++ add_del_usuarios.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+OPERATION=`cat $1 | cut -d',' -f1`
+USERNAME=`cat $1 | cut -d',' -f2`
+
+if [ $OPERATION == add ]
+then
+      if [ $USERNAME == `cat /etc/passwd | cut -d':' -f1` ]
+      echo "Creando usuario $USERNAME ..."
+      adduser $USERNAME
