7a7ff5815e788d049a87a8fe6fa4ea935cc2054d
llorenzogo <34598954+llorenzogo@users.noreply.github.com>
2018-02-28 00:15:09 +0100

    Update add_del_usuarios.sh

--- add_del_usuarios.sh
+++ add_del_usuarios.sh
@@ -5,6 +5,21 @@ USERNAME=`cat $1 | cut -d',' -f2`
 
 if [ $OPERATION == add ]
 then
-      if [ $USERNAME == `cat /etc/passwd | cut -d':' -f1` ]
+      if [ $USERNAME != `cat /etc/passwd | cut -d':' -f1` ]
+      then
       echo "Creando usuario $USERNAME ..."
       adduser $USERNAME
+      else
+      exit
+      fi
+fi
+if [ $OPERATION == del] 
+then	
+if [ $USERNAME == `cat /etc/passwd | cut -d':' -f1 ] 
+then
+echo "Borrando usuario $USERNAME ..." 
+userdel $USERNAME 
+else
+exit
+fi
+fi
